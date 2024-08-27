6dcf419b712ccc145bd732690bb68a5d534d674c
abdul <abdul.tanveer@gmail.com>
2024-08-27 10:59:18 +0530

    client querying and passing arguments

--- client.js
+++ client.js
@@ -1,10 +1,37 @@
-fetch(" http://localhost:5000/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ query: "{ hello }" }),
-  })
-    .then(r => r.json())
-    .then(data => console.log("data returned:", data))
+// fetch(" http://localhost:5000/graphql", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify({ query: "{ hello }" }),
+//   })
+//     .then(r => r.json())
+//     .then(data => console.log("data returned:", data))
+
+var dice = 3
+var sides = 6
+var query = /* GraphQL */`query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`
+
+
+fetch("http://localhost:5000/graphql", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  }),
+})
+  .then(r => r.json())
+  .then(data => console.log("data returned:", data))
+
+
+
+//   {
+//     rollDice(numDice: 3, numSides: 6)
+//   }
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var { ruruHTML } = require("ruru/server")
 var schema = buildSchema(`
   type Query {
     hello: String
+    rollDice(numDice: Int!, numSides: Int): [Int]
+
   }
 `)
  
@@ -18,6 +20,14 @@ var root = {
   hello() {
     return "Hello world!"
   },
+  rollDice(args) {
+    var output = []
+    for (var i = 0; i < args.numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)))
+    }
+    return output
+  },
+
 }
  
 var app = express()
